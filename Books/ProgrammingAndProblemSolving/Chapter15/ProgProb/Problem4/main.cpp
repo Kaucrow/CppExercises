c5222e161fefa08f2ebcbed98db2873d385b5e79
kaucrow <kaucrow@protonmail.com>
2023-08-26 22:13:32 -0400

    Chapter 15 Problem 4 completed. Code needs cleanup

--- Books/ProgrammingAndProblemSolving/Chapter15/ProgProb/Problem4/main.cpp
+++ Books/ProgrammingAndProblemSolving/Chapter15/ProgProb/Problem4/main.cpp
@@ -7,29 +7,29 @@ using   std::cerr, std::cout, std::cin, std::string,
 const int MAX_LENGTH = 100;
 
 int main(){
-    /*stringstream outFileSS;
+    stringstream outFileSS;
     string outFileName;
     char exitFlag;
-    system("del /s *.dat");
-    ofstream contactOut;
+    //system("del /s *.dat");
+    ofstream outFile;
     GenSortedList* contactList;
-
+    /*
     // *** GENERATE THE contacts_.dat FILES ***
     for(int i = 0; i < MAX_LENGTH; i++){
         exitFlag = '0';
         outFileSS << "contacts" << i + 1 << ".dat"; outFileName = outFileSS.str();
-        contactOut.open(outFileName);
+        outFile.open(outFileName);
         
         // LIST GETS SORTED BY PATERNAL SURNAME. If contacts with no paternal surname are introduced, they
         // will be at the top of the outFile and the ones that do have a paternal surname will get sorted and
         // written after them
         contactList = new GenSortedList(MAX_LENGTH, 1);
         std::cout << *contactList;
-        contactOut << *contactList;
+        outFile << *contactList;
  
         delete contactList; contactList = nullptr;
         outFileSS.str(""); outFileSS.clear();   
-        contactOut.close();
+        outFile.close();
         
         while(exitFlag != 'y' && exitFlag != 'n' && i < MAX_LENGTH - 1){
             cout << "Create a new contacts file? (y/n): "; exitFlag = cin.get();
@@ -42,29 +42,38 @@ int main(){
     int filesNum, contactsNum = 0;
     stringstream inFileSS;
     string inFileName, tempStr;
-    ifstream contactIn;
+    ifstream inFile;
     cout << "How many files will be merged?: "; cin >> filesNum;
     int contactsNumArr[filesNum];
     memset(contactsNumArr, 0, sizeof(contactsNumArr));
     
     for(int i = 0; i < filesNum; i++){
         inFileSS << "contacts" << i + 1 << ".dat"; inFileName = inFileSS.str();
-        contactIn.open(inFileName);
+        inFile.open(inFileName);
+        if(!inFile){ cerr << "ERR: FILE \"" << inFileName << "\" COULD NOT BE OPENED\n"; }
 
-        std::getline(contactIn, tempStr);
+        std::getline(inFile, tempStr);
         contactsNumArr[i] += stoi(tempStr.substr(tempStr.find_first_of("123456789")));
         contactsNum += contactsNumArr[i];
 
         inFileSS.str(""); inFileSS.clear();
-        contactIn.close();
+        inFile.close();
     }
-    cout << contactsNumArr[0];
-    cout << contactsNumArr[1];
-    cout << contactsNum;
+
     OutSortedList outList(contactsNum);
-    outList.GetFileContacts("contacts1.dat", contactsNumArr, 0);
-    outList.GetFileContacts("contacts2.dat", contactsNumArr, 1);
+    for(int i = 0; i < filesNum; i++){
+        inFileSS << "contacts" << i + 1 << ".dat"; inFileName = inFileSS.str();
+        inFile.open(inFileName);
+        if(!inFile){ cerr << "ERR: FILE \"" << inFileName << "\" COULD NOT BE OPENED\n"; }
+
+        outList.GetFileContacts(inFileName, contactsNumArr, i);
+
+        inFileSS.str(""); inFileSS.clear();
+        inFile.close();
+    }
+
+    outFile.open("mergedcontacts.dat");
     outList.Reset();
-    cout << outList;
-    //outList.Test();
+    outFile << outList;
+    outFile.close();
 }
